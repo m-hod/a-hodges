9ea785b065e4abc21177363f6184f2ecd98f3f12
m-hodges <michaelcshodges@gmail.com>
2021-10-20 01:33:57 +1300

    fix hero carousel on mobile

--- components/wrappers/hero.tsx
+++ components/wrappers/hero.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Circle } from "react-feather";
 import BackgroundImage from "../elements/BackgroundImage";
+import { Circle } from "react-feather";
+import React from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -10,7 +10,7 @@ interface Props {
 function Hero({ children, headerHeight }: Props) {
   return (
     <div
-      className="w-full relative flex"
+      className="w-full relative flex overflow-x-hidden"
       style={{
         height: `calc(100vh - ${headerHeight || 0}px)`,
       }}
