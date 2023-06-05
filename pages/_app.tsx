7ef88b6eb2e13501565d4a4f3026e8eeba115283
mhod <mhod@tutanota.com>
2023-06-05 21:06:52 +1200

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
   const AnyComponent = Component as any;
-  return <AnyComponent {...pageProps} />;
+  return (
+    <>
+      <AnyComponent {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
